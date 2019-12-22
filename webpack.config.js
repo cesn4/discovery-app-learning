2aefca5daf30a488ba3d7a9a8c5a4a97604f08cc
Rokas <rokas.anisas@gmail.com>
2019-12-22 19:54:57 +0200

    css loader

--- webpack.config.js
+++ webpack.config.js
@@ -10,6 +10,14 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js'
   },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ]
+  },
   plugins: [
     new HtmlWebpackPlugin({
       title: 'React app',
