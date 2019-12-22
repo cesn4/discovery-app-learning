7527a9ef6c10b696058ee93cba998494e8c333b2
Rokas <rokas.anisas@gmail.com>
2019-12-22 18:45:27 +0200

    webpack config adjustments

---
+++ webpack.config.js
@@ -0,0 +1,18 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'React app',
+    }),
+    new CleanWebpackPlugin()
+  ]
+};
