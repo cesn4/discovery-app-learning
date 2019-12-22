5b33d15d2e195f6d14e07e3761039387970e68e7
Rokas <rokas.anisas@gmail.com>
2019-12-22 20:11:57 +0200

    Template html file

--- webpack.config.js
+++ webpack.config.js
@@ -30,6 +30,7 @@ module.exports = {
   plugins: [
     new HtmlWebpackPlugin({
       title: 'React app',
+      template: path.resolve(__dirname, 'public/index.html'),
     }),
     new CleanWebpackPlugin(),
     new BrowserSyncPlugin({
