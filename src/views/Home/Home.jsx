32eaca712acba25aef5a70a7765790409f2ac726
Rokas <rokas.anisas@gmail.com>
2019-12-30 19:02:43 +0200

    Footer component

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
 import Header from '~/components/Header';
+import Footer from '~/components/Footer';
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
             Home view
+            <Footer />
         </div>
     )
 };
