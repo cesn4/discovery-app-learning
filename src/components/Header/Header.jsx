f7b911ec42215a68cb07f640128612a5bbd711e6
Rokas <rokas.anisas@gmail.com>
2019-12-30 18:52:56 +0200

    Hotfix component name

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -2,10 +2,10 @@ import React from 'react';
 
 import './Header.scss';
 
-const Home = () => {
+const Header = () => {
     return (
         <header className="header">Header</header>
     )
 };
 
-export default Home;
+export default Header;
