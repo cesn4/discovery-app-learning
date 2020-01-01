c4190c970b3e033ea927c76d319c467a4680723c
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-02 00:56:57 +0200

    almost finished header for tablet

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,6 +7,12 @@ const Header = () => {
         <header className="header">
             <div className="menu-icon"></div>
             <div className="logo">LOGO</div>
+            <ul className="headerMenu">
+                <li className="menuHome">Home</li>
+                <li className="menuDiscovery">Discovery</li>
+                <li className="menuPhotos">Photos</li>
+                <li className="menuContact">Contact</li>
+            </ul>
             <div className="photo"></div>
         </header>
     );
