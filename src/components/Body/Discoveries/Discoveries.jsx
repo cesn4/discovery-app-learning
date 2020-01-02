f967f78fb9f50d3befda1fff2e980b83b3cfe560
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-02 05:40:19 +0200

    Discovery box styling complete with minor bugs
    Paveiksleliai isikelia su borderiais keistai kai tvarkysiuos img ikelimus tad susitvarkysiu :D

--- src/components/Body/Discoveries/Discoveries.jsx
+++ src/components/Body/Discoveries/Discoveries.jsx
@@ -5,28 +5,31 @@ import './Discoveries.scss';
 const Discoveries = () => {
     return (
         <div className="discoveriesBox">
-            <div className="titleBox">
-                <h2 className="title">Discoveries</h2>
-                <img className="arrowButtons"></img>
-            </div>
-            <div className="discConBox">
-                <div className="discTravel">
-                    <div className="travelTitle" >Travel</div>
-                    <div className="traveltitle">Article title</div>
-                    <p className="travelInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
-                    <ul className="travelTime">
-                        <img className="travelClock"></img>
-                        <li className="travelTimeCounter">2m ago</li>
-                    </ul>
+            <div className="discoText">
+                <div className="titleBox">
+                    <h2 className="title">Discoveries</h2>
+                    <img className="arrowButtonsLeft"></img>
+                    <img className="arrowButtonsRight"></img>
                 </div>
-                <div className="discTech">
-                    <div className="techTitle" >Technology</div>
-                    <div className="techtitle">Article title</div>
-                    <p className="techInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
-                    <ul className="techTime">
-                        <img className="techClock"></img>
-                        <li className="techTimeCounter">1h ago</li>
-                    </ul>
+                <div className="discConBox">
+                    <div className="discTravel">
+                        <div className="travelTitle" >Travel</div>
+                        <div className="travelArticTitle">Article title</div>
+                        <p className="travelInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
+                        <ul className="travelTime">
+                            <img className="travelClock"></img>
+                            <p className="travelTimeCounter">2m ago</p>
+                        </ul>
+                    </div>
+                    <div className="discTech">
+                        <div className="techTitle" >Technology</div>
+                        <div className="techArticTitle">Article title</div>
+                        <p className="techInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
+                        <ul className="techTime">
+                            <img className="techClock"></img>
+                            <p className="techTimeCounter">1h ago</p>
+                        </ul>
+                    </div>
                 </div>
             </div>
         </div>
