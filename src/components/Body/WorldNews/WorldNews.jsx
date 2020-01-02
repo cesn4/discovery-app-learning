205b1bf035070532ac1d52bbbb4cadf5ec016704
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-02 03:56:25 +0200

    World News finished

--- src/components/Body/WorldNews/WorldNews.jsx
+++ src/components/Body/WorldNews/WorldNews.jsx
@@ -7,9 +7,9 @@ const WorldNews = () => {
         <div className="newsBox">
             <div className="worldNewsText">
                 <h1 className="world-news">World News</h1>
-                <h2 className="newsTitle"></h2>
-                <p className="newsInfromation"></p>
-                <a className="button" href="-">Learn More</a>
+                <h2 className="newsTitle">Amazing places in America to visit</h2>
+                <p className="newsInfromation">For some reason — this country, this city, this neighborhood, this particular street —  is the place you are living a majority of your life in.</p>
+                <button className="learnButton">Learn More</button>
             </div>
         </div>
     );
