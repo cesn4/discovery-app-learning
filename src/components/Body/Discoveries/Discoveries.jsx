ce1f618083bf7176788643424805dd72091b7297
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-02 04:27:10 +0200

    Discovery boxxx creation

--- src/components/Body/Discoveries/Discoveries.jsx
+++ src/components/Body/Discoveries/Discoveries.jsx
@@ -4,7 +4,32 @@ import './Discoveries.scss';
 
 const Discoveries = () => {
     return (
-        <div className="discoveries">Discoveries</div>
+        <div className="discoveriesBox">
+            <div className="titleBox">
+                <h2 className="title">Discoveries</h2>
+                <img className="arrowButtons"></img>
+            </div>
+            <div className="discConBox">
+                <div className="discTravel">
+                    <div className="travelTitle" >Travel</div>
+                    <div className="traveltitle">Article title</div>
+                    <p className="travelInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
+                    <ul className="travelTime">
+                        <img className="travelClock"></img>
+                        <li className="travelTimeCounter">2m ago</li>
+                    </ul>
+                </div>
+                <div className="discTech">
+                    <div className="techTitle" >Technology</div>
+                    <div className="techtitle">Article title</div>
+                    <p className="techInfo">Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…</p>
+                    <ul className="techTime">
+                        <img className="techClock"></img>
+                        <li className="techTimeCounter">1h ago</li>
+                    </ul>
+                </div>
+            </div>
+        </div>
     );
 };
 
