1cd054555afa54742a52517df9c30608f05412d2
Rokas <rokas.anisas@gmail.com>
2020-01-02 09:20:36 +0200

    File loader added

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import image from '~/assets/image.jpg';
 import './Header.scss';
 
 const Header = () => {
     return (
-        <header className="header">Header</header>
+        <header className="header">
+            Header
+            <img className="header__image" src={image} alt=""/>
+            <div className="header__bgi"></div>
+        </header>
     );
 };
 
--- webpack.config.js
+++ webpack.config.js
@@ -47,6 +47,18 @@ module.exports = {
                 }
             }
             ]
+        },
+        {
+            test: /\.(jpg|png|svg|gif)$/,
+            use: [
+                {
+                    loader: 'file-loader',
+                    options: {
+                        outputPath: 'img/',
+                        name: '[name].[hash].[ext]'
+                    }
+                }
+            ]
         }
         ]
     },
