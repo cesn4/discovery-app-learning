2a42fcb8a5f183cc600004249ef2b21066a12e6e
Rokas <rokas.anisas@gmail.com>
2020-01-02 09:25:07 +0200

    Moved example to Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
 
+import image from '~/assets/image.jpg';
 import './Footer.scss';
 
 const Footer = () => {
     return (
-        <footer className="footer">Footer</footer>
+        <footer className="footer">
+            Footer
+            <img className="footer__image" src={image} alt="" />
+            <div className="footer__bgi"></div>
+        </footer>
     );
 };
 
--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,14 +1,11 @@
 import React from 'react';
 
-import image from '~/assets/image.jpg';
 import './Header.scss';
 
 const Header = () => {
     return (
         <header className="header">
             Header
-            <img className="header__image" src={image} alt=""/>
-            <div className="header__bgi"></div>
         </header>
     );
 };
