197b42facc09087b557e669ac7def244ac83e06d
Rokas <rokas.anisas@gmail.com>
2020-01-02 11:35:47 +0200

    stylelint rules added

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -7,6 +7,10 @@ module.exports = {
         'color-no-invalid-hex': true,
         'indentation': 4,
         'color-named': 'never',
-        'order/properties-alphabetical-order': true
+        'order/properties-alphabetical-order': true,
+        'block-closing-brace-newline-after': 'always',
+        'selector-max-empty-lines': 0,
+        'block-opening-brace-newline-after': 'always-multi-line',
+        'rule-empty-line-before': 'always-multi-line'
     }
 }
