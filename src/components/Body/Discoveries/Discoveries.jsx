4d0f973df87f768273c8d6d645b6d071bd58baf9
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-02 04:05:04 +0200

    Created Discoveries folder

---
+++ src/components/Body/Discoveries/Discoveries.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Discoveries.scss';
+
+const Discoveries = () => {
+    return (
+        <div className="discoveries">Discoveries</div>
+    );
+};
+
+export default Discoveries;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,12 +3,14 @@ import React from 'react';
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import WorldNews from '~/components/Body/WorldNews';
+import Discoveries from '~/components/Body/Discoveries';
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
             <WorldNews />
+            <Discoveries />
             Home view
             <Footer />
         </div>
