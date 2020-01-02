77f7f878b60fd356a9d6a1197e1a1d339f91bda5
Rokas <rokas.anisas@gmail.com>
2020-01-02 11:20:07 +0200

    stylelint attribute order

--- .stylelintrc.js
+++ .stylelintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
     extends: 'stylelint-config-recommended-scss',
+    plugins: [
+        'stylelint-order'
+    ],
     rules: {
         'color-no-invalid-hex': true,
         'indentation': 4,
         'color-named': 'never',
+        'order/properties-alphabetical-order': true
     }
 }
