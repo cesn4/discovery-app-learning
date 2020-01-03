83d1e9fd952b324339b372652d1a9418dc381a2d
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-03 14:12:10 +0200

    Blocks and structure

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,7 +5,19 @@ import './Hero.scss';
 const Hero = () => {
     return (
         <div className="hero">
-            Hero
+            <div className="hero__news">
+                <div className="news">
+                    <h1 className="news__main-title">WORLD NEWS</h1>
+                    <h2 className="news__second-title">Amazing places in America to visit.</h2>
+                    <p className="news__paragraph">For some reason — this country, this city, this neighborhood, this particular street — is the place you are living a majority of your life in.</p>
+                    <span className="news__button">LEARN MORE</span>
+                </div>
+            </div>
+            <div className="hero__discoveries">
+                <div className="discoveries">
+                    <h1 className="discoveries__title">DISCOVERIES</h1>
+                </div>
+            </div>
         </div>
     );
 };
