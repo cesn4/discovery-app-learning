f79ab69c5292553d74dbd137380dae069e0d3880
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-03 15:30:24 +0200

    stylinga

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -7,8 +7,10 @@ const Hero = () => {
         <div className="hero">
             <div className="hero__news">
                 <div className="news">
-                    <h1 className="news__main-title">WORLD NEWS</h1>
-                    <h2 className="news__second-title">Amazing places in America to visit.</h2>
+                    <div className="news__title">
+                        <span>WORLD NEWS</span>
+                    </div>
+                    <h2 className="news__subtitle">Amazing places in America to visit.</h2>
                     <p className="news__paragraph">For some reason — this country, this city, this neighborhood, this particular street — is the place you are living a majority of your life in.</p>
                     <span className="news__button">LEARN MORE</span>
                 </div>
