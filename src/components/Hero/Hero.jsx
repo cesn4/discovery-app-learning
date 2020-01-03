9977266456e7e19756a8f9b27d52d1cd5f47b4c9
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-03 13:12:09 +0200

    Created dir and undated Dependencies

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './Hero.scss';
+
+const Hero = () => {
+    return (
+        <div className="hero">
+            Hero
+        </div>
+    );
+};
+
+export default Hero;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
+import Hero from '~/components/Hero';
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
+            <Hero />
             Home view
             <Footer />
         </div>
