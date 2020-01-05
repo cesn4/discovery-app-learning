ba6cf054056277683056f739081133d798ec31ef
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-05 05:04:18 +0200

    news styled
    Nervina biski tas konteineris kolkas, negaliu pasidaryti taip kaip reikia man :D, galvoju gal virsu pasidaryti manualiai, o po to su gridais.

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 
+import '~/assets/mount.jpg';
 import './Hero.scss';
 
 const Hero = () => {
     return (
         <div className="hero">
             <Container>
-                <Row>
-                    <Col sm={12} md={6}>
+                <Row >
+                    <Col xl={6} className="background">
                         <div className="hero__news">
                             <div className="news">
                                 <div className="news__title">
                                     <span>WORLD NEWS</span>
                                 </div>
-                                <h2 className="news__subtitle">Amazing places in America to visit.</h2>
+                                <span className="news__subtitle">Amazing places in America to visit.</span>
                                 <p className="news__paragraph">For some reason — this country, this city, this neighborhood, this particular street — is the place you are living a majority of your life in.</p>
-                                <span className="news__button">LEARN MORE</span>
+                                <a className="news__button">LEARN MORE</a>
                             </div>
                         </div>
                     </Col>
-                    <Col sm={12} md={6}>
+                    <Col sm={12} md={12} xl={6}>
                         <div className="hero__discoveries">
                             <div className="discoveries">
                                 <h1 className="discoveries__title">DISCOVERIES</h1>
