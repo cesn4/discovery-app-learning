4b5cce7adf642d34accf1c526fee7ad411c4f4cd
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-08 04:34:19 +0200

    classNames foe Hero fixed

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -13,12 +13,12 @@ const Hero = () => {
                 <Row >
                     <Col sm={12} md={12} xl={7} className="hero__background">
                         <div className="hero__news">
-                            <div className="news">
-                                <div className="news__title-box">
-                                    <span className="news_title">WORLD NEWS</span>
+                            <div className="hero__news-box">
+                                <div className="hero__title-box">
+                                    <span className="hero_title">WORLD NEWS</span>
                                 </div>
-                                <span className="news__subtitle">Amazing places in America to visit.</span>
-                                <p className="news__paragraph">For some reason — this country, this city, this neighborhood, this particular street — is the place you are living a majority of your life in.</p>
+                                <span className="hero__subtitle">Amazing places in America to visit.</span>
+                                <p className="hero__paragraph">For some reason — this country, this city, this neighborhood, this particular street — is the place you are living a majority of your life in.</p>
                                 <SimpleButton buttonName="LEARN MORE" />
                             </div>
                         </div>
