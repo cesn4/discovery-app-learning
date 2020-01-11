8440d799bf6a63696a1fb9a4d8176fdbb1edf77e
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-11 12:34:53 +0200

    Hero moved to sections

--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
-import Hero from '~/components/Hero';
+import Hero from '~/sections/Hero';
 
 const Home = () => {
     return (
