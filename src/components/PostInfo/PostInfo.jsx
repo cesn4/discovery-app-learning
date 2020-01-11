993ca2befdb6f3a56fc997bc87b97ca4ea319648
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-11 11:54:10 +0200

    minor changes and class to const

--- src/components/PostInfo/PostInfo.jsx
+++ src/components/PostInfo/PostInfo.jsx
@@ -5,26 +5,24 @@ import '~/assets/img/time.png';
 
 import './PostInfo.scss';
 
-class PostInfo extends React.Component {
-    render () {
-        return (
-            <div className="post">
-                <span className="post__title">{this.props.title}</span>
-                <span className="post__subtitle">{this.props.subtitle}</span>
-                <p className="post__text">{this.props.text}</p>
-                <div className="post__time">
-                    <i className="post__clock"></i>
-                    <span className="post__clock-time">{this.props.time}</span>
-                </div>
+const PostInfo = ({ title, subtitle, primaryText, time }) => {
+    return (
+        <div className="post">
+            <span className="post__title">{title}</span>
+            <span className="post__subtitle">{subtitle}</span>
+            <p className="post__text">{primaryText}</p>
+            <div className="post__time">
+                <i className="post__clock"></i>
+                <span className="post__clock-time">{time}</span>
             </div>
-        );
-    }
-}
+        </div>
+    );
+};
 
 PostInfo.propTypes = {
     title: PropTypes.string.isRequired,
     subtitle: PropTypes.string.isRequired,
-    text: PropTypes.string.isRequired,
+    primaryText: PropTypes.string.isRequired,
     time: PropTypes.string.isRequired
 };
 
