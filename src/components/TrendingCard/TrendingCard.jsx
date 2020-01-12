d5b942d5100e3c2fb733266f8ad48bf179c803b6
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-12 18:19:43 +0200

    boxes and props created for Cards

--- src/components/TrendingCard/TrendingCard.jsx
+++ src/components/TrendingCard/TrendingCard.jsx
@@ -1,11 +1,28 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import './TrendingCard.scss';
 
-const TrendingCard = () => {
+const TrendingCard = ({ title, time, paragraphText }) => {
     return (
-        <div className="main"></div>
+        <div className="card">
+            <div className="card__background"></div>
+            <div className="card__text-box">
+                <span className="card__title">{title}</span>
+                <p className="card__paragraph-text">{paragraphText}</p>
+            </div>
+            <div className="post__time">
+                <i className="post__clock"></i>
+                <span className="post__clock-time">{time}</span>
+            </div>
+        </div>
     );
+};
+
+TrendingCard.propTypes = {
+    title: PropTypes.string.isRequired,
+    paragraphText: PropTypes.string.isRequired,
+    time: PropTypes.string.isRequired
 };
 
 export default TrendingCard;
