7ec15e51d36760cd573d8c49fc9d5e2cb993eb8d
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-12 05:20:29 +0200

    created knight section

---
+++ src/sections/Knight/Knight.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Knight.scss';
+
+const Knight = () => {
+    return (
+        <div className="main"></div>
+    );
+};
+
+export default Knight;
--- src/views/Home/Home.jsx
+++ src/views/Home/Home.jsx
@@ -3,13 +3,14 @@ import React from 'react';
 import Header from '~/components/Header';
 import Footer from '~/components/Footer';
 import Hero from '~/sections/Hero';
+import Knight from '~/sections/Knight';
 
 const Home = () => {
     return (
         <div className="home">
             <Header />
             <Hero />
-            Home view
+            <Knight />
             <Footer />
         </div>
     );
