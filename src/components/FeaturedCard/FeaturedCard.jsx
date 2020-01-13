7aed8afceb50ced8fe40297d8d6a9b675b325a17
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-13 03:55:48 +0200

    classses for cards

---
+++ src/components/FeaturedCard/FeaturedCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+import './FeaturedCard.scss';
+
+const FeaturedCard = () => {
+    return (
+        <div className="featured-card">Card
+            <div className="featured-card__tex-box">
+                <span className="featured-card__theme"></span>
+                <span className="featured-card__title"></span>
+                <p className="featured-card__paragraph"></p>
+            </div>
+        </div>
+    );
+};
+
+export default FeaturedCard;
--- src/sections/Featured/Featured.jsx
+++ src/sections/Featured/Featured.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 
+import FeaturedCard from '~/components/FeaturedCard';
 import SectionNavigation from '~/components/SectionNavigation';
 
 import './Featured.scss';
@@ -12,6 +13,9 @@ const Featured = () => {
                 <Row>
                     <Col sm={12} md={12} xl={12}>
                         <SectionNavigation borderless title="Happening Now"/>
+                    </Col>
+                    <Col>
+                        <FeaturedCard />
                     </Col>
                 </Row>
             </Container>
