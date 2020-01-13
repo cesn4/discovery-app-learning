690755628e4984095a5ac46347283102aabd71d0
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-13 03:43:46 +0200

    Section Navigation added

--- src/sections/Featured/Featured.jsx
+++ src/sections/Featured/Featured.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+
+import SectionNavigation from '~/components/SectionNavigation';
 
 import './Featured.scss';
 
 const Featured = () => {
     return (
-        <div className="featured"></div>
+        <div className="featured">
+            <Container>
+                <Row>
+                    <Col sm={12} md={12} xl={12}>
+                        <SectionNavigation borderless title="Happening Now"/>
+                    </Col>
+                </Row>
+            </Container>
+        </div>
     );
 };
 
