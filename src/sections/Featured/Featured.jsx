407288f028e5a2bb597073fc9627a7422e45c782
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-14 10:55:08 +0200

    finall layout for Featured section

--- src/sections/Featured/Featured.jsx
+++ src/sections/Featured/Featured.jsx
@@ -24,10 +24,18 @@ const Featured = () => {
                         <FeaturedCard firstItem background={hand} theme="CITY" title="Large article title mobile layout" paragraph="Lorem ipsum dolor sit amet, in eam odio amet, vix id nullam detracto, vidit vituperatoribus duo id. Affert detraxit voluptatum vis eu, inermis eloquentiam." time="2m ago"/>
                         <FeaturedCard lastItem background={house} theme="TRAVEL" title="Large article title mobile layout" paragraph="Lorem ipsum dolor sit amet, in eam odio amet, vix id nullam detracto, vidit vituperatoribus duo id. Affert detraxit voluptatum vis eu, inermis eloquentiam." time="2m ago" />
                     </Col>
-                    <Col sm={12} md={6} xl={4} className="featured__small-cards">
-                        <SmallCard firstItem image={wall} title="Small title" time="3h ago by Worldnews"/>
-                        <SmallCard image={castle} title="Small title" time="4h ago by Days" />
-                        <SmallCard hidden image={random} title="Small title" time="4h ago by Days" />
+                    <Col sm={12} md={12} xl={4} className="featured__small-cards">
+                        <Row>
+                            <Col md={6} xl={12}>
+                                <SmallCard firstItem image={wall} title="Small title" time="3h ago by Worldnews"/>
+                            </Col>
+                            <Col md={6} xl={12}>
+                                <SmallCard image={castle} title="Small title" time="4h ago by Days" />
+                            </Col>
+                            <Col>
+                                <SmallCard hidden image={random} title="Small title" time="4h ago by Days" />
+                            </Col>
+                        </Row>
                     </Col>
                 </Row>
             </Container>
