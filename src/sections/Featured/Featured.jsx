4d0898007e443a945d9e576e8142e653f741fa1c
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-14 10:03:45 +0200

    layout fixes

--- src/sections/Featured/Featured.jsx
+++ src/sections/Featured/Featured.jsx
@@ -9,6 +9,7 @@ import castle from '~/assets/img/castle.jpg';
 import wall from '~/assets/img/wall.jpg';
 import house from '~/assets/img/house.jpg';
 import hand from '~/assets/img/hand.jpg';
+import random from '~/assets/img/random.jpg';
 import './Featured.scss';
 
 const Featured = () => {
@@ -21,15 +22,12 @@ const Featured = () => {
                     </Col>
                     <Col sm={12} md={12} xl={8}>
                         <FeaturedCard firstItem background={hand} theme="CITY" title="Large article title mobile layout" paragraph="Lorem ipsum dolor sit amet, in eam odio amet, vix id nullam detracto, vidit vituperatoribus duo id. Affert detraxit voluptatum vis eu, inermis eloquentiam." time="2m ago"/>
-                    </Col>
-                    <Col sm={12} md={12} xl={8}>
                         <FeaturedCard lastItem background={house} theme="TRAVEL" title="Large article title mobile layout" paragraph="Lorem ipsum dolor sit amet, in eam odio amet, vix id nullam detracto, vidit vituperatoribus duo id. Affert detraxit voluptatum vis eu, inermis eloquentiam." time="2m ago" />
                     </Col>
-                    <Col sm={12} md={6} xl={4}>
+                    <Col sm={12} md={6} xl={4} className="featured__small-cards">
                         <SmallCard firstItem image={wall} title="Small title" time="3h ago by Worldnews"/>
-                    </Col>
-                    <Col sm={12} md={6} xl={4}>
                         <SmallCard image={castle} title="Small title" time="4h ago by Days" />
+                        <SmallCard image={random} title="Small title" time="4h ago by Days" />
                     </Col>
                 </Row>
             </Container>
