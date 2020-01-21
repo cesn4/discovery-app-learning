81064cc5546039c5befb9f68d5f2d522e0ac9936
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-21 11:22:56 +0200

    fixes

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
                         </Col>
                     </Hidden>
                     <Hidden xs>
-                        <Col md={1.5} xl={1}>
+                        <Col md={1} xl={1}>
                             <ul className="footer__columns">
                                 <li className="footer__item">Home</li>
                                 <li className="footer__item">Discovery</li>
@@ -30,7 +30,7 @@ const Footer = () => {
                             </ul>
                         </Col>
                     </Hidden>
-                    <Col xs={6} md={1.5} xl={1} >
+                    <Col xs={6} md={1} xl={1} >
                         <ul className="footer__columns">
                             <li className="footer__item">About</li>
                             <li className="footer__item">Help</li>
