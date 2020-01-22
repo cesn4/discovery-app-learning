687718ec59fd4804a5d553ea9758cce06dc48b18
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-01-22 11:49:03 +0200

    Props added to NavigationItem component

--- src/components/NavigationItem/NavigationItem.jsx
+++ src/components/NavigationItem/NavigationItem.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import classNames from 'classnames';
 
 import './NavigationItem.scss';
 
-const NavigationItem = () => {
+const NavigationItem = ({ href, icon, title }) => {
     return (
         <div className="navigation-item">
-            <a href="#" className="sidebar__nav-anchor"><img src={home} className="sidebar__img"/><li className="sidebar__nav-item -active">Home</li></a>
+            <a href={href} className="navigation-item__anchor">
+                <img src={icon} className="navigation-item__icon"/>
+                <span className={classNames('navigation-item__title', { '-active': active })}>{title}</span>
+            </a>
         </div>
     );
 };
+
+NavigationItem.propTypes = {
+    href: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    title: PropTypes.string.isRequired,
+    active: PropTypes.bool
+}
+
+NavigationItem.defaultProps = {
+    active: false
+}
 
 export default NavigationItem;
