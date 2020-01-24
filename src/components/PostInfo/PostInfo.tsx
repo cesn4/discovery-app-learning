87948c4582da3b7c3250dd4f12b60e997d85c6f1
Rokas <rokas.anisas@gmail.com>
2020-01-24 10:53:56 +0200

    PostInfo prop changed to ts

--- src/components/PostInfo/PostInfo.tsx
+++ src/components/PostInfo/PostInfo.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import PropTypes from 'prop-types';
 
 import Time from '~/components/Time';
 
 import '~/assets/img/time.png';
 import './PostInfo.scss';
 
-const PostInfo = ({ title, subtitle, primaryText, time }) => {
+const PostInfo = ({ title, subtitle, primaryText, time }: PostInfoProps) => {
     return (
         <div className="post-info">
             { title && <span className="post-info__title">{title}</span>}
@@ -17,15 +16,11 @@ const PostInfo = ({ title, subtitle, primaryText, time }) => {
     );
 };
 
-PostInfo.propTypes = {
-    title: PropTypes.string,
-    subtitle: PropTypes.string.isRequired,
-    primaryText: PropTypes.string.isRequired,
-    time: PropTypes.string.isRequired
-};
-
-PostInfo.defaultProps = {
-    title: undefined
-};
+interface PostInfoProps {
+    title?: string;
+    subtitle: string;
+    primaryText: string;
+    time: string;
+}
 
 export default PostInfo;
