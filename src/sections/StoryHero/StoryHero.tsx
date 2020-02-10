00cc4da35abb8e92465820939e1b79682d972fb9
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-11 00:25:04 +0200

    StoryHero componenet created

---
+++ src/sections/StoryHero/StoryHero.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+
+import './StoryHero.scss';
+
+import '~/assets/img/jungle.jpg';
+
+const StoryHero: FunctionComponent = () => {
+    return (
+        <div className="story-hero">Something from nothing
+        </div>
+    );
+};
+
+export default StoryHero;
--- src/views/Story/Story.tsx
+++ src/views/Story/Story.tsx
@@ -1,12 +1,14 @@
 import React, { FunctionComponent } from 'react';
 
 import Header from '~/components/Header';
+import StoryHero from '~/sections/StoryHero';
 import Footer from '~/components/Footer';
 
 const Story: FunctionComponent = () => {
     return (
         <div className="story">
             <Header />
+            <StoryHero />
             <Footer />
         </div>
     );
