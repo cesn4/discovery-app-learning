7c1aa4754f3916e2b3e8608251db8c7e251b19e4
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-11 00:02:30 +0200

    deleted unnecessary configs

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,6 @@ module.exports = {
     output: {
         path: path.resolve(__dirname, "dist"),
         filename: "bundle.[hash].js",
-        publicPath: '/'
     },
     module: {
         rules: [
@@ -77,15 +76,6 @@ module.exports = {
             ]
         }
         ]
-    },
-    devServer: {
-        contentBase: 'app/ui/www',
-        devtool: 'eval',
-        hot: true,
-        inline: true,
-        port: 3000,
-        outputPath: buildPath,
-        historyApiFallback: true,
     },
     plugins: [
         new HtmlWebpackPlugin({
