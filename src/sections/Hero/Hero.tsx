b6ab2a354dafe6ca625a22cdc2a893b27e16eb85
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-10 22:50:38 +0200

    fixed Hero postInfo layout

--- src/sections/Hero/Hero.tsx
+++ src/sections/Hero/Hero.tsx
@@ -16,25 +16,27 @@ const Hero: FunctionComponent = () => {
                         <Background />
                     </Col>
                     <Col xs={12} md={12} lg={5}>
-                        <div className="hero__section-lable">
-                            <SectionNavigation title="Discoveries" />
-                        </div>
-                        <div className="hero__posts">
-                            <div className="hero__first-post">
-                                <PostInfo
-                                    title="Travel"
-                                    subtitle="Article title"
-                                    primaryText="Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…"
-                                    time="2m ago"
-                                />
+                        <div className="hero__post-box">
+                            <div className="hero__section-lable">
+                                <SectionNavigation title="Discoveries" />
                             </div>
-                            <div className="hero__second-post">
-                                <PostInfo
-                                    title="Technology"
-                                    subtitle="Article title"
-                                    primaryText="Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…"
-                                    time="1h ago"
-                                />
+                            <div className="hero__posts">
+                                <div className="hero__first-post">
+                                    <PostInfo
+                                        title="Travel"
+                                        subtitle="Article title"
+                                        primaryText="Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…"
+                                        time="2m ago"
+                                    />
+                                </div>
+                                <div className="hero__second-post">
+                                    <PostInfo
+                                        title="Technology"
+                                        subtitle="Article title"
+                                        primaryText="Lorem ipsum dolor sit amet, ipsum labitur lucilius mel id, ad has appareat…"
+                                        time="1h ago"
+                                    />
+                                </div>
                             </div>
                         </div>
                     </Col>
