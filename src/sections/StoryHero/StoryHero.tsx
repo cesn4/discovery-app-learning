bd0c7e9c092ceb9ff50d17f39272bef155b83852
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-11 02:45:51 +0200

    SoryHero layout for mobile

--- src/sections/StoryHero/StoryHero.tsx
+++ src/sections/StoryHero/StoryHero.tsx
@@ -17,31 +17,38 @@ const StoryHero: FunctionComponent = () => {
     return (
         <div className={className}>
             <Container>
-                <Row>
-                    <Col xs={12} md={12} lg={12}>
-                        <div className={`${className}__title`}>
-                            AMAZING PLACES IN AMERICA TO VISIT
-                        </div>
-                    </Col>
-                    <Col>
-                        <div className={`${className}__icon-box`}>
-                            <div className={`${className}__icon-label`}>
-                                <IconLabel label="TRAVELGUIDE" name="web" />
+                <div className={`${className}__box`}>
+                    <Row>
+                        <Col xs={12} md={12} lg={12}>
+                            <div className={`${className}__title`}>
+                                AMAZING PLACES IN AMERICA TO VISIT
                             </div>
-                            <div className={`${className}__icon-label`}>
-                                <IconLabel label="WORLD NEWS" name="topic" />
+                        </Col>
+                        <Col>
+                            <div className={`${className}__icon-box`}>
+                                <div className={`${className}__icon-label`}>
+                                    <IconLabel label="TRAVELGUIDE" name="web" />
+                                </div>
+                                <div className={`${className}__icon-label`}>
+                                    <IconLabel
+                                        label="WORLD NEWS"
+                                        name="topic"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                    </Col>
-                </Row>
-                <Row>
-                    <Col>
-                        <Author image={AuthorPhoto} />
-                    </Col>
-                    <Col>
-                        <NextStory />
-                    </Col>
-                </Row>
+                        </Col>
+                    </Row>
+                    <Row align="center">
+                        <Col xs={6}>
+                            <Author image={AuthorPhoto} />
+                        </Col>
+                        <Col xs={6}>
+                            <div className={`${className}__next-story`}>
+                                <NextStory />
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </Container>
         </div>
     );
