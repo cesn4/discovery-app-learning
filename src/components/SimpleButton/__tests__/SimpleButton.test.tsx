b92cc47ab44b27c92419a7480eab0a7144f2af16
Rokas <rokas.anisas@gmail.com>
2020-02-16 00:58:45 +0200

    [ 44 ] minor test description adjustments

--- src/components/SimpleButton/__tests__/SimpleButton.test.tsx
+++ src/components/SimpleButton/__tests__/SimpleButton.test.tsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import SimpleButton from '../SimpleButton';
 
 describe('Time component', () => {
-    it('Should render text prop', () => {
+    it('Should render title', () => {
         const testTest = 'Test text';
         const wrapper = shallow(<SimpleButton title={testTest} />);
         expect(wrapper.find('.button__title').text()).toEqual(testTest);
--- src/components/Time/__tests__/Time.test.tsx
+++ src/components/Time/__tests__/Time.test.tsx
@@ -3,7 +3,7 @@ import { shallow } from 'enzyme';
 import Time from '../Time';
 
 describe('Time component', () => {
-    it('Should render text prop', () => {
+    it('Should render text', () => {
         const testTest = 'Test text';
         const wrapper = shallow(<Time text={testTest} />);
         expect(wrapper.find('.time__clock-time').text()).toEqual(testTest);
