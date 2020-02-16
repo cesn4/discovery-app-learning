cc0d0b109f354077c7d306df4953c39a8bba11d7
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-16 03:21:42 +0200

    SVGProps fixed + cts-transition

--- src/components/Icons/assets/SVGFacebook.tsx
+++ src/components/Icons/assets/SVGFacebook.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import SVGIconProps from '~/types';
+import { SVGIconProps } from '~/types';
 
 const SVGFacebook: FunctionComponent<SVGIconProps> = ({
     color,
