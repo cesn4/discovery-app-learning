8f8babb70f93ec8a201c2751f4be907bca0953da
Rokas <rokas.anisas@gmail.com>
2020-02-16 14:01:37 +0200

    [ 44 ] Extended SectionNavigation test

--- src/components/SectionNavigation/__tests__/SectionNavigation.test.tsx
+++ src/components/SectionNavigation/__tests__/SectionNavigation.test.tsx
@@ -2,12 +2,39 @@ import React from 'react';
 import { shallow } from 'enzyme';
 import SectionNavigation from '../SectionNavigation';
 
+const testTest = 'Test text';
+
 describe('SectionNavigation component', () => {
     it('Should render title', () => {
-        const testTest = 'Test text';
         const wrapper = shallow(<SectionNavigation title={testTest} />);
         expect(wrapper.find('.section-navigation__title').text()).toEqual(
             testTest
         );
+    });
+
+    it('Should render borderless', () => {
+        const wrapper = shallow(
+            <SectionNavigation title={testTest} borderless />
+        );
+        expect(
+            wrapper.find('.section-navigation').hasClass('-borderless')
+        ).toBeTruthy();
+        expect(
+            wrapper
+                .find('.section-navigation__title-box')
+                .hasClass('-borderless')
+        ).toBeTruthy();
+    });
+
+    it('Should render with border', () => {
+        const wrapper = shallow(<SectionNavigation title={testTest} />);
+        expect(
+            wrapper.find('.section-navigation').hasClass('-borderless')
+        ).toBeFalsy();
+        expect(
+            wrapper
+                .find('.section-navigation__title-box')
+                .hasClass('-borderless')
+        ).toBeFalsy();
     });
 });
