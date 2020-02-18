8e436ed6d913cd055f4d947fabaf397c65399207
Rokas <rokas.anisas@gmail.com>
2020-02-18 17:08:10 +0200

    updated simple component

--- src/components/SimpleComponent/SimpleComponent.tsx
+++ src/components/SimpleComponent/SimpleComponent.tsx
@@ -2,14 +2,17 @@ import React, { FunctionComponent } from 'react';
 
 interface SimpleComponentProps {
     text: string;
+    active?: boolean;
 }
 
 const SimpleComponent: FunctionComponent<SimpleComponentProps> = ({
     text,
+    active = false,
 }: SimpleComponentProps) => {
     return (
         <div className="simple-component">
             <h1>{text}</h1>
+            {active && <h2>Shite</h2>}
         </div>
     );
 };
