9d773a4775623d81702ecd5b42f12a2fb4b6da99
Rokas <rokas.anisas@gmail.com>
2020-02-19 17:37:39 +0200

    [ 47 ] ts true

--- .doczrc.js
+++ .doczrc.js
@@ -1,6 +1,7 @@
 const webpackConfig = require('./webpack.config.js');
 
 module.exports = {
+    typescript: true,
     modifyBundlerConfig: config => {
         config.resolve.alias = {
             ...config.resolve.alias,
