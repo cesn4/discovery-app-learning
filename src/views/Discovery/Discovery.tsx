68079d81af2d0709cb994d7e9fba5b53e8433441
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-19 05:46:11 +0200

    Discovery view/route created

---
+++ src/views/Discovery/Discovery.tsx
@@ -0,0 +1,14 @@
+import React, { FunctionComponent } from 'react';
+
+import PageLayout from '~/layouts/PageLayout';
+
+const Discovery: FunctionComponent = () => {
+    const className = 'discovery';
+    return (
+        <div className={className}>
+            <PageLayout />
+        </div>
+    );
+};
+
+export default Discovery;
