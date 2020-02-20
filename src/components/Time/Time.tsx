01d65b407b9605ad4d36d9aad20d8beee71391ba
Rokas <rokas.anisas@gmail.com>
2020-02-20 13:39:27 +0200

    [ 47 ] color value updated

--- src/components/Time/Time.tsx
+++ src/components/Time/Time.tsx
@@ -8,7 +8,7 @@ const Time: FunctionComponent<TimeProps> = ({ text }: TimeProps) => {
     return (
         <div className="time">
             {/* TODO: replace hardcoded color value with a scss Colors import variable */}
-            <Icon className="time__clock" name="time" color="#a6adb4" />
+            <Icon className="time__clock" name="time" color="#ccd1d4" />
             <span className="time__clock-time">{text}</span>
         </div>
     );
