037592fc0dea436fe5bb609fac62c5cba0fad7b4
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-25 19:13:35 +0200

    DiscoverySearch section created

---
+++ src/sections/DiscoverySearch/DiscoverySearch.tsx
@@ -0,0 +1,17 @@
+import React, { FunctionComponent } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
+
+const DiscoverySearch: FunctionComponent = () => {
+    const className = 'discovery-search';
+    return (
+        <div className={className}>
+            <Container>
+                <Row>
+                    <Col></Col>
+                </Row>
+            </Container>
+        </div>
+    );
+};
+
+export default DiscoverySearch;
--- src/views/Discovery/Discovery.tsx
+++ src/views/Discovery/Discovery.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent, Fragment } from 'react';
 
 import PageLayout from '~/layouts/PageLayout';
 import DiscoveryHero from '~/sections/DiscoveryHero';
+import DiscoverySearch from '~/sections/DiscoverySearch';
 
 const Discovery: FunctionComponent = () => {
     const className = 'discovery';
@@ -11,6 +12,7 @@ const Discovery: FunctionComponent = () => {
                 content={
                     <Fragment>
                         <DiscoveryHero />
+                        <DiscoverySearch />
                     </Fragment>
                 }
             />
