87b8eb062b530240ad0b1c9f0415325a5578fc54
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-27 00:57:09 +0200

    AuthorNotes component created + content

---
+++ src/components/AuthorNotes/AuthorNotes.tsx
@@ -0,0 +1,33 @@
+import React, { FunctionComponent } from 'react';
+
+import Author from '~/components/Author';
+import AuthorPost from '~/components/AuthorPost';
+
+import './AuthorNotes.scss';
+
+const AuthorNotes: FunctionComponent<AuthorNotesProps> = ({
+    image,
+    title,
+    post,
+    time,
+}: AuthorNotesProps) => {
+    const className = 'author-notes';
+    return (
+        <div className={className}>
+            <div className={`${className}__title-box`}>
+                <Author image={image} />
+                <span className={`${className}__title`}>{title}</span>
+            </div>
+            <AuthorPost post={post} time={time} />
+        </div>
+    );
+};
+
+interface AuthorNotesProps {
+    image: string;
+    title: string;
+    post: string;
+    time: string;
+}
+
+export default AuthorNotes;
--- src/sections/DiscoveryKnight/DiscoveryKnight.tsx
+++ src/sections/DiscoveryKnight/DiscoveryKnight.tsx
@@ -1,6 +1,9 @@
 import React, { FunctionComponent } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 
+import AuthorNotes from '~/components/AuthorNotes';
+
+import AuthorPhoto from '~/assets/img/author.jpg';
 import './DiscoveryKnight.scss';
 
 const DisocveryKNight: FunctionComponent = () => {
@@ -10,7 +13,14 @@ const DisocveryKNight: FunctionComponent = () => {
             <Container>
                 <Row>
                     <Col>
-                        <div className={`${className}__content`}>Hello</div>
+                        <div className={`${className}__content`}>
+                            <AuthorNotes
+                                image={AuthorPhoto}
+                                title="Phasellus eu nulla malesuada, luctus diam."
+                                post="Dignissim rutrum ridiculus lacinia phasellus torquent ad aliquet, nisi dictum cubilia class habitasse commodo, semper potenti nec ac per egestas. Ac volutpat ullamcorper phasellus montes sollicitudin litora ridiculus mi conubia inceptos euismod odio curabitur, tortor eros porta venenatis facilisis quam blandit in ut lobortis consequat justo. Hac libero quisque tortor conubia iaculis. Nascetur himenaeos morbi gravida porta sapien justo aliquam pellentesque dapibus curae, cursus ultrices suspendisse cras ligula id aenean vulputate taciti, eleifend eros bibendum scelerisque lobortis venenatis nulla tristique tempus. Quisque eleifend vulputate."
+                                time="2 min ago"
+                            />
+                        </div>
                     </Col>
                 </Row>
             </Container>
