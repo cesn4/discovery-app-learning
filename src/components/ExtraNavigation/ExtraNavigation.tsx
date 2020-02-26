e2426d1403e756302b11c447d9b0ce51baff39fd
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-26 18:23:08 +0200

    React grid system added

--- src/components/ExtraNavigation/ExtraNavigation.tsx
+++ src/components/ExtraNavigation/ExtraNavigation.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 
 import './ExtraNavigation.scss';
 import Icon from '../Icons';
@@ -7,12 +8,20 @@ const ExtraNavigation: FunctionComponent = () => {
     const className = 'extra-navigation';
     return (
         <div className={className}>
-            <a href="#" className={`${className}__back-box`}>
-                <Icon name="back" size={16} color="#343638" />
-            </a>
-            <a href="#" className={`${className}__filter-box`}>
-                <Icon name="filter" size={16} color="#343638" />
-            </a>
+            <Container>
+                <Row>
+                    <Col>
+                        <div className={`${className}__box`}>
+                            <a href="#" className={`${className}__back-box`}>
+                                <Icon name="back" size={16} color="#343638" />
+                            </a>
+                            <a href="#" className={`${className}__filter-box`}>
+                                <Icon name="filter" size={16} color="#343638" />
+                            </a>
+                        </div>
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
