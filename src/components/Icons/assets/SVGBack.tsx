b0b40d9067818ed5d42c8b1f1747c1e3932d6646
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-26 18:57:48 +0200

    Deleted unnecessary  divs

--- src/components/Icons/assets/SVGBack.tsx
+++ src/components/Icons/assets/SVGBack.tsx
@@ -6,24 +6,22 @@ const SVGBack: FunctionComponent<SVGIconProps> = ({
     size,
 }: SVGIconProps) => {
     return (
-        <div>
-            <svg
-                className="svg-icon"
-                width={size}
-                height={size}
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox={`0 0 ${size} ${size}`}
-            >
-                <g data-name="Arrow Back" transform="translate(-36 -92)">
-                    <path
-                        fill={color}
-                        d="M16 7H3.8l5.6-5.6L8 0 0 8l8 8 1.4-1.4L3.8 9H16z"
-                        data-name="Arrow Back"
-                        transform="translate(36 92)"
-                    />
-                </g>
-            </svg>
-        </div>
+        <svg
+            className="svg-icon"
+            width={size}
+            height={size}
+            xmlns="http://www.w3.org/2000/svg"
+            viewBox={`0 0 ${size} ${size}`}
+        >
+            <g data-name="Arrow Back" transform="translate(-36 -92)">
+                <path
+                    fill={color}
+                    d="M16 7H3.8l5.6-5.6L8 0 0 8l8 8 1.4-1.4L3.8 9H16z"
+                    data-name="Arrow Back"
+                    transform="translate(36 92)"
+                />
+            </g>
+        </svg>
     );
 };
 
--- src/components/Icons/assets/SVGFilter.tsx
+++ src/components/Icons/assets/SVGFilter.tsx
@@ -6,23 +6,21 @@ const SVGFilter: FunctionComponent<SVGIconProps> = ({
     size,
 }: SVGIconProps) => {
     return (
-        <div>
-            <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width={size}
-                height={size}
-                viewBox={`0 0 ${size} ${size}`}
-            >
-                <g transform="translate(-1462 -104)">
-                    <path
-                        fill={color}
-                        d="M16 1v2H5.7a1.945 1.945 0 0 1-3.4 0H0V1h2.3a1.945 1.945 0 0 1 3.4 0zm-4 5a1.936 1.936 0 0 0-1.7 1H0v2h10.3a1.945 1.945 0 0 0 3.4 0H16V7h-2.3A1.936 1.936 0 0 0 12 6zm-4 6a1.936 1.936 0 0 0-1.7 1H0v2h6.3a1.945 1.945 0 0 0 3.4 0H16v-2H9.7A1.936 1.936 0 0 0 8 12z"
-                        data-name="filter"
-                        transform="translate(1462 104)"
-                    />
-                </g>
-            </svg>
-        </div>
+        <svg
+            xmlns="http://www.w3.org/2000/svg"
+            width={size}
+            height={size}
+            viewBox={`0 0 ${size} ${size}`}
+        >
+            <g transform="translate(-1462 -104)">
+                <path
+                    fill={color}
+                    d="M16 1v2H5.7a1.945 1.945 0 0 1-3.4 0H0V1h2.3a1.945 1.945 0 0 1 3.4 0zm-4 5a1.936 1.936 0 0 0-1.7 1H0v2h10.3a1.945 1.945 0 0 0 3.4 0H16V7h-2.3A1.936 1.936 0 0 0 12 6zm-4 6a1.936 1.936 0 0 0-1.7 1H0v2h6.3a1.945 1.945 0 0 0 3.4 0H16v-2H9.7A1.936 1.936 0 0 0 8 12z"
+                    data-name="filter"
+                    transform="translate(1462 104)"
+                />
+            </g>
+        </svg>
     );
 };
 
