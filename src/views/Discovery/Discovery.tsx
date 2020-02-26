33102d9f8f3fd32847b0fff84bca444e6b85dd37
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-26 19:34:11 +0200

    Additional Layout for discovery view created

--- src/views/Discovery/Discovery.tsx
+++ src/views/Discovery/Discovery.tsx
@@ -1,13 +1,12 @@
 import React, { FunctionComponent } from 'react';
 
-import PageLayout from '~/layouts/PageLayout';
-import ExtraNavigation from '~/components/ExtraNavigation';
+import DiscoveryLayout from '~/layouts/DsicoveryLayout';
 
 const Discovery: FunctionComponent = () => {
     const className = 'discovery';
     return (
         <div className={className}>
-            <PageLayout content={<ExtraNavigation label="Discovery" />} />
+            <DiscoveryLayout />
         </div>
     );
 };
