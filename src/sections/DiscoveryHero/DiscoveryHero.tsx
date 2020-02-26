4908e0d4ada17ee4fe89eabb33c645276ad7c57a
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-26 17:31:02 +0200

    DiscoveryHero background passed throuth prop

--- src/sections/DiscoveryHero/DiscoveryHero.tsx
+++ src/sections/DiscoveryHero/DiscoveryHero.tsx
@@ -8,7 +8,12 @@ import Dude from '~/assets/img/dude.jpg';
 import '~/assets/img/DiscoveryMountain.jpg';
 import './DiscoveryHero.scss';
 
-const DiscoveryHero: FunctionComponent = () => {
+const DiscoveryHero: FunctionComponent<DiscoveryHeroProps> = ({
+    background,
+}: DiscoveryHeroProps) => {
+    const styleBackground: DiscoveryBackgroundObject = {
+        backgroundImage: 'url(' + background + ')',
+    };
     const className = 'discovery-hero';
     return (
         <div className={className}>
@@ -16,7 +21,10 @@ const DiscoveryHero: FunctionComponent = () => {
                 <div className={`${className}__box`}>
                     <Row>
                         <Col xs={12} md={12} lg={6}>
-                            <div className={`${className}__content`}>
+                            <div
+                                className={`${className}__content`}
+                                style={styleBackground}
+                            >
                                 <DiscoveryTitle
                                     title="DISCOVERY"
                                     subtitle="Lorem ipsum dolor sit amet, in eam odio amet, vix id nullam detracto vidit."
@@ -30,5 +38,13 @@ const DiscoveryHero: FunctionComponent = () => {
         </div>
     );
 };
+
+interface DiscoveryHeroProps {
+    background: string;
+}
+
+interface DiscoveryBackgroundObject {
+    backgroundImage: string;
+}
 
 export default DiscoveryHero;
