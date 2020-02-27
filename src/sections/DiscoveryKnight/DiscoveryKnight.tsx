452c7c2d8c3c225d006c6df6ec7ddb8b3788173d
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-27 02:25:02 +0200

    Separators added to section

--- src/sections/DiscoveryKnight/DiscoveryKnight.tsx
+++ src/sections/DiscoveryKnight/DiscoveryKnight.tsx
@@ -2,6 +2,7 @@ import React, { FunctionComponent } from 'react';
 import { Container, Row, Col } from 'react-grid-system';
 
 import AuthorNotes from '~/components/AuthorNotes';
+import Separator from '~/components/Separator';
 
 import AuthorPhoto from '~/assets/img/author.jpg';
 import './DiscoveryKnight.scss';
@@ -14,12 +15,14 @@ const DisocveryKNight: FunctionComponent = () => {
                 <Row>
                     <Col>
                         <div className={`${className}__content`}>
+                            <Separator />
                             <AuthorNotes
                                 image={AuthorPhoto}
                                 title="Phasellus eu nulla malesuada, luctus diam."
                                 post="Dignissim rutrum ridiculus lacinia phasellus torquent ad aliquet, nisi dictum cubilia class habitasse commodo, semper potenti nec ac per egestas. Ac volutpat ullamcorper phasellus montes sollicitudin litora ridiculus mi conubia inceptos euismod odio curabitur, tortor eros porta venenatis facilisis quam blandit in ut lobortis consequat justo. Hac libero quisque tortor conubia iaculis. Nascetur himenaeos morbi gravida porta sapien justo aliquam pellentesque dapibus curae, cursus ultrices suspendisse cras ligula id aenean vulputate taciti, eleifend eros bibendum scelerisque lobortis venenatis nulla tristique tempus. Quisque eleifend vulputate."
                                 time="2 min ago"
                             />
+                            <Separator />
                         </div>
                     </Col>
                 </Row>
