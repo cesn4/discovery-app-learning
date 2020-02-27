9bcdc278f16bb43897ed6275e0162452aaf51257
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-02-27 02:12:10 +0200

    Desktop view

--- src/components/AuthorNotes/AuthorNotes.tsx
+++ src/components/AuthorNotes/AuthorNotes.tsx
@@ -1,4 +1,5 @@
 import React, { FunctionComponent } from 'react';
+import { Container, Row, Col } from 'react-grid-system';
 
 import Author from '~/components/Author';
 import Article from '../Article';
@@ -14,11 +15,21 @@ const AuthorNotes: FunctionComponent<AuthorNotesProps> = ({
     const className = 'author-notes';
     return (
         <div className={className}>
-            <div className={`${className}__title-box`}>
-                <Author image={image} big />
-                <span className={`${className}__title`}>{title}</span>
-            </div>
-            <Article content={post} time={time} notes />
+            <Container>
+                <Row>
+                    <Col xs={12} md={12} lg={4}>
+                        <div className={`${className}__title-box`}>
+                            <Author image={image} big />
+                            <span className={`${className}__title`}>
+                                {title}
+                            </span>
+                        </div>
+                    </Col>
+                    <Col xs={12} md={12} lg={8}>
+                        <Article content={post} time={time} notes />
+                    </Col>
+                </Row>
+            </Container>
         </div>
     );
 };
