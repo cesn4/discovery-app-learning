1915a74678dec7ec417ce1318efcb7496d6c0f1a
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-03-06 13:04:02 +0200

    aditional test

--- src/components/ExtraNavigation/__tests__/ExtraNavigation.test.tsx
+++ src/components/ExtraNavigation/__tests__/ExtraNavigation.test.tsx
@@ -1,15 +1,17 @@
-import React from 'react';
-import { shallow } from 'enzyme';
 import ExtraNavigation, {
     ExtraNavigationProps,
 } from '~/components/ExtraNavigation';
+
+import React from 'react';
+import { shallow } from 'enzyme';
 
 const testLabel = 'Test Label';
 const testHref = '#';
 
 const HappyProps: ExtraNavigationProps = {
     label: testLabel,
-    href: testHref,
+    backHref: testHref,
+    homeHref: testHref,
 };
 
 describe('ExtraNavigation component', () => {
@@ -18,10 +20,17 @@ describe('ExtraNavigation component', () => {
         expect(wrapper.find('.extra-navigation__label').text()).toBe(testLabel);
     });
 
-    it('Should render ExtraNavigation href', () => {
+    it('Should render ExtraNavigation back Icon href', () => {
         const wrapper = shallow(<ExtraNavigation {...HappyProps} />);
         expect(
             wrapper.find('.extra-navigation__back-anchor').prop('href')
+        ).toBe(testHref);
+    });
+
+    it('Should render ExtraNavigation Home icon href', () => {
+        const wrapper = shallow(<ExtraNavigation {...HappyProps} />);
+        expect(
+            wrapper.find('.extra-navigation__home-anchor').prop('href')
         ).toBe(testHref);
     });
 });
