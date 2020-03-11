51fcb1c2a0a20c3c4aa71e6fd04251c5292963ed
cesn4 <59119122+cesn4@users.noreply.github.com>
2020-03-12 01:54:08 +0200

    SmallCards generation from second Half of Discovery Pages

---
+++ src/containers/SmallCardsContainer/SmallCardsContainer.tsx
@@ -0,0 +1,62 @@
+import React, { FunctionComponent, Fragment } from 'react';
+import { connect } from 'react-redux';
+import { Col, Hidden } from 'react-grid-system';
+
+import { ApplicationState } from '~/store/storeTypes';
+import { DiscoveryPages } from '~/containers/DiscoveriesContainer';
+import SmallCard from '~/components/SmallCard';
+
+const SmallCardsContainer: FunctionComponent<DiscoveryPages> = ({
+    discoveryContent,
+}: DiscoveryPages) => {
+    const renderSmallCards = discoveryContent.map(
+        ({ id, title, background }: SmallCardItems, index) => {
+            if (id >= 3) {
+                let className = '';
+                if (id === 3) {
+                    className = 'featured__small-card -first';
+                } else if (id === 4) {
+                    className = 'featured__small-card -second';
+                } else if (id === 5) {
+                    className = 'featured__small-card -third';
+                }
+                let hiddenState = false;
+                if (id === 5) {
+                    hiddenState = true;
+                }
+                return (
+                    <Fragment key={index.toString()}>
+                        <Hidden sm={hiddenState} md={hiddenState}>
+                            <Col md={6} lg={12}>
+                                <div className={className}>
+                                    <SmallCard
+                                        image={background}
+                                        title={title}
+                                        time="4h ago by Days"
+                                    />
+                                </div>
+                            </Col>
+                        </Hidden>
+                    </Fragment>
+                );
+            } else {
+                return <Fragment></Fragment>;
+            }
+        }
+    );
+    return <Fragment>{renderSmallCards}</Fragment>;
+};
+
+const mapStateToProps = (state: ApplicationState): DiscoveryPages => {
+    return {
+        discoveryContent: state.discoveryContent,
+    };
+};
+
+interface SmallCardItems {
+    id: number;
+    title: string;
+    background: string;
+}
+
+export default connect(mapStateToProps)(SmallCardsContainer);
--- src/sections/Featured/Featured.tsx
+++ src/sections/Featured/Featured.tsx
@@ -1,14 +1,10 @@
 import React, { FunctionComponent } from 'react';
-import { Container, Row, Col, Hidden } from 'react-grid-system';
+import { Container, Row, Col } from 'react-grid-system';
 
 import SectionNavigation from '~/components/SectionNavigation';
-import SmallCard from '~/components/SmallCard';
-
-import castle from '~/assets/img/castle.jpg';
-import wall from '~/assets/img/wall.jpg';
-import random from '~/assets/img/random.jpg';
 import './Featured.scss';
 import FeaturedCardsContainer from '~/containers/FeaturedContainer';
+import SmallCardsContainer from '~/containers/SmallCardsContainer';
 
 const Featured: FunctionComponent = () => {
     return (
@@ -31,36 +27,7 @@ const Featured: FunctionComponent = () => {
                         </Col>
                         <Col xs={12} md={12} lg={4}>
                             <Row>
-                                <Col md={6} lg={12}>
-                                    <div className="featured__small-card -first">
-                                        <SmallCard
-                                            firstItem
-                                            image={wall}
-                                            title="Small title"
-                                            time="3h ago by Worldnews"
-                                        />
-                                    </div>
-                                </Col>
-                                <Col md={6} lg={12}>
-                                    <div className="featured__small-card -second">
-                                        <SmallCard
-                                            image={castle}
-                                            title="Small title"
-                                            time="4h ago by Days"
-                                        />
-                                    </div>
-                                </Col>
-                                <Hidden sm md>
-                                    <Col>
-                                        <div className="featured__small-card -third">
-                                            <SmallCard
-                                                image={random}
-                                                title="Small title"
-                                                time="4h ago by Days"
-                                            />
-                                        </div>
-                                    </Col>
-                                </Hidden>
+                                <SmallCardsContainer />
                             </Row>
                         </Col>
                     </Row>
